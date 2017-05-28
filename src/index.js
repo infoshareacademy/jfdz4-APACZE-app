90353c6428723a3c6bac9102a6aa0eba2dc8f147
arturdrawc <artur.drawc@gmail.com>
2017-05-28 12:08:42 +0200

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap-theme.css'
+
+import App from './App'
 
 ReactDOM.render(
   <App />,
